01ce8fbf8b44a43741d8efa3253912d87d3eb2b9
Dwayne <dewilliams0327@gmail.com>
2019-10-22 23:15:59 -0500

    add pictures and animation

--- views/js/script.js
+++ views/js/script.js
@@ -1,21 +1,37 @@
-//
-// start here
-//
+
 function main() {
-    const canvas = document.querySelector("#glCanvas");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+
+  var lhand = document.getElementById('clock-lhand');
+  var bhand = document.getElementById('clock-bhand');
+
+  let time = new Date()
+  let mins = ((time.getMinutes()+time.getSeconds()/100)*6)%360 
   
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-      return;
-    }
-  
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  lhand.style.transform = `rotate(${mins}deg)`;
+  bhand.style.transform = `rotate(${(((time.getHours()%12)*30)+(mins/100))%360}deg)`;
+
+  setInterval(()=>{ 
+    time = new Date()
+    mins = ((time.getMinutes()+time.getSeconds()/100)*6)%360
+    lhand.style.transform = `rotate(${mins}deg)`;
+    bhand.style.transform = `rotate(${(((time.getHours()%12)*30)+(mins/100))%360}deg)`;
+    // img.style.bottom = `${lhand.baseY+lhand.rot/lhand.baseY}px`;
+    // img.style.left = `${lhand.currX++}px`;
+    // console.log(img.style.bottom)
+    
+  }, 1000);
+
+  let yRot = 0
+  let yDir = false
+  var anime = document.getElementById('anime');
+  var shadow = document.getElementById('shadow');
+  setInterval(()=>{
+    if(Math.floor(yRot)===2||Math.floor(yRot)===-2)
+      yDir=!yDir
+    anime.style.transform = `rotate(${yDir?yRot+=.05:yRot-=.05}deg)`;
+    shadow.style.left=`${-yRot}px`
+  }, 50);
+
 }
 
 window.onload = main;
