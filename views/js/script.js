900eb10aa544313fa70b9bfcd67e5ab356cfd91d
Dwayne <dewilliams0327@gmail.com>
2019-10-24 23:12:44 -0500

    still churning

--- views/js/script.js
+++ views/js/script.js
@@ -6,18 +6,22 @@ function main() {
 
   let time = new Date()
   let mins = ((time.getMinutes()+time.getSeconds()/100)*6)%360 
+  let hours = (((time.getHours()%12)*30)+(mins/100))%360
   
   lhand.style.transform = `rotate(${mins}deg)`;
-  bhand.style.transform = `rotate(${(((time.getHours()%12)*30)+(mins/100))%360}deg)`;
+  bhand.style.transform = `rotate(${hours}deg)`;
+  // bhand.style.height= `${37-(hours+500)/100}%`
+  // bhand.style.top= `${14-(hours-1100)/100}%`
 
   setInterval(()=>{ 
     time = new Date()
     mins = ((time.getMinutes()+time.getSeconds()/100)*6)%360
+    hours= (((time.getHours()%12)*30)+(mins/100))%360
+    
     lhand.style.transform = `rotate(${mins}deg)`;
-    bhand.style.transform = `rotate(${(((time.getHours()%12)*30)+(mins/100))%360}deg)`;
-    // img.style.bottom = `${lhand.baseY+lhand.rot/lhand.baseY}px`;
-    // img.style.left = `${lhand.currX++}px`;
-    // console.log(img.style.bottom)
+    bhand.style.transform = `rotate(${hours}deg)`;
+    // bhand.style.height= `${37-(hours+500)/100}%`
+    // bhand.style.top= `${14-(hours-1100)/100}%`
     
   }, 1000);
 
@@ -26,9 +30,9 @@ function main() {
   var anime = document.getElementById('anime');
   var shadow = document.getElementById('shadow');
   setInterval(()=>{
-    if(Math.round(yRot*10)/10===1.4||Math.round(yRot*10)/10===-1.4)
+    if(Math.round(yRot*10)/10===1||Math.round(yRot*10)/10===-1)
       yDir=!yDir
-    anime.style.transform = `rotate(${yDir?yRot+=.02:yRot-=.02}deg)`;
+    anime.style.transform = `rotate(${yDir?yRot+=.04:yRot-=.04}deg)`;
     shadow.style.left=`${-yRot}px`
   }, 50);
 
