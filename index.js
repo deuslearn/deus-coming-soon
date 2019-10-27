f6587528d109210941aa22602ee6980dbd201bb1
Dwayne <dewilliams0327@gmail.com>
2019-10-27 16:32:35 -0500

    add site homepage

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8084;
+
+const PAGE = process.env.PAGE || "homepage"
 
 app.set('view engine', 'pug')
 app.set('env production');
@@ -12,7 +14,7 @@ app.locals.basedir = app.get('views');
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render(PAGE);
 });
 
 app.listen(PORT)
