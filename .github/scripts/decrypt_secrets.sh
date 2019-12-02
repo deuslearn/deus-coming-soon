44cf76f70bb7e5304b1fe50fbeeb7a24a2153faf
Dwayne <dewilliams0327@gmail.com>
2019-12-01 20:22:11 -0600

    re arrange things

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -5,4 +5,4 @@ echo "$DEUS_KEY"
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase "$DEUS_KEY" \
---output .github/deploy/app.yml .github/deploy/app.yml.gpg
+--output app.yml app.yml.gpg
